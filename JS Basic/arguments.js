30e73edb29cbb9838f4f9c549dfefc9ea8983530
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-01-25 18:03:15 +0600

    update event

---
+++ JS Basic/arguments.js
@@ -0,0 +1,10 @@
+function addNumbers(num1, num2 ) {
+    var sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        const num = arguments[i];
+        sum += num;
+    }
+    return sum;
+}
+var res = addNumbers(3, 5, 10, 2, 20);
+console.log(res);
